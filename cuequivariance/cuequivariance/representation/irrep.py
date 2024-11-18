6e1ddce95a0efef2ef77bd4a5c44d7139f32629f
Mario Geiger <mgeiger@nvidia.com>
2024-11-18 08:45:25 +0100

    headers (#22)

--- cuequivariance/cuequivariance/representation/irrep.py
+++ cuequivariance/cuequivariance/representation/irrep.py
@@ -1,5 +1,17 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from __future__ import annotations
 
 import dataclasses
@@ -153,7 +165,7 @@ def clebsch_gordan(rep1: Irrep, rep2: Irrep, rep3: Irrep) -> np.ndarray:
         array([[[[ 0.  ...]]]])
 
         If there is no solution, the output is an empty array.
-        
+
         >>> C = clebsch_gordan(cue.SO3(1), cue.SO3(1), cue.SO3(3))
         >>> C.shape
         (0, 3, 3, 7)
--- cuequivariance_torch/cuequivariance_torch/primitives/tensor_product.py
+++ cuequivariance_torch/cuequivariance_torch/primitives/tensor_product.py
@@ -1,5 +1,17 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import logging
 import math
 import warnings
@@ -68,7 +80,7 @@ class TensorProduct(torch.nn.Module):
                 input is not on CUDA. If `True`, a PyTorch fallback method is used regardless of CUDA kernel availability.
 
         Returns:
-            torch.Tensor: 
+            torch.Tensor:
                 The output tensor resulting from the tensor product.
                 It has a shape of (batch, last_operand_size), where
                 `last_operand_size` is the size of the last operand in the descriptor.
