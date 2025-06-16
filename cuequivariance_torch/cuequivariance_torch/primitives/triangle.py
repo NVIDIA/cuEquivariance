5a03a07fb594499df594cf82b0ce55ab54832012
hari sadasivan <hsadasivan@nvidia.com>
2025-06-16 15:08:19 -0700

    importin trimulprecision

--- cuequivariance_torch/cuequivariance_torch/primitives/triangle.py
+++ cuequivariance_torch/cuequivariance_torch/primitives/triangle.py
@@ -12,14 +12,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Optional, Tuple
+from typing import Any, Optional, Tuple
 
 import torch
 
 try:
     from cuequivariance_ops_torch import TriMulPrecision
 except ImportError:
-    TriMulPrecision = None  # type: ignore
+    TriMulPrecision = Any  # type: ignore
 
 
 def triangle_attention(
